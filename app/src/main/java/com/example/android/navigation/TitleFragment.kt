e1bd29a0dfdc2ca0c8fe0bb6a5c59ad758ecac1d
valyakuttan <valyakuttan@users.noreply.github.com>
2020-05-19 22:53:38 +0530

    step 06 completed

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -17,13 +17,12 @@
 package com.example.android.navigation
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -37,16 +36,26 @@ class TitleFragment : Fragment() {
         // Right click on the res folder within the Android project and select New Resource File
         // Name it overflow_menu with resource type menu.  Add an About menu item with the ID of
         // the aboutFragment.
+
         // TODO (03) Call setHasOptionsMenu(true)
         // This tells Android that our fragment has an Options Menu, so it will call
         // onCreateOptionsMenu
+        setHasOptionsMenu(true)
+
         return binding.root
     }
 
     // TODO (04) Override onCreateOptionsMenu
     // Use the passed-in MenuInflater to inflate the overflow_menu
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
 
     // TODO (05) Override onOptionsItemSelected
     // Return true if NavigationUI.onNavDestinationSelected returns true, else return
     // super.onOptionsItemSelected.
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!, view!!.findNavController()) ||
+                super.onOptionsItemSelected(item)
+    }
 }
